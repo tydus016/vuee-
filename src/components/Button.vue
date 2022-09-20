880a0eb429b8c4c33e1661e5c593b46bd0c19662
tydus016 <eomegajr@gmail.com>
2022-09-20 17:53:26 +0800

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button 
+    :style="{background: color}" 
+    class="btn" 
+    @click="onClick()">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            'text': String,
+            'color': String
+        },
+        methods: {
+            onClick(){
+                console.log('??')
+            }
+        }
+    }
+</script>
